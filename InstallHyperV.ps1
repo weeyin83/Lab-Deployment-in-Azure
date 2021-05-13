10de94d9d3277cf60a108ff7776b732901e16c1e
Sarah <salean@microsoft.com>
2021-05-13 16:56:46 +0100

    TLS support

--- InstallHyperV.ps1
+++ InstallHyperV.ps1
@@ -16,6 +16,7 @@ Microsoft Lab Environment
 #>
 
 Set-ExecutionPolicy Unrestricted -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls1
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
 Find-Module -Includes DscResource -Name xHyper-v | Install-Module -Force
 
